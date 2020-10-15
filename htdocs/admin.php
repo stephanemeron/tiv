404b8a633680cb59025b4ec3053352168bd9fcac
smeron@lesechos.fr <smeron@lesechos.fr>
2020-10-15 17:33:31 +0200

    transformation html en bs4

--- htdocs/admin.php
+++ htdocs/admin.php
@@ -1,39 +1,61 @@
-<h2>Déclaration d'un nouvel élément dans la base</h2>
-<p><img src='images/configure.png' style="float: left; margin: 0 15px 0 0;" />
-<form name="ajout_form" id="ajout_form" action="ajout_element.php" method="POST">
-Type d'élément à déclarer :
-<select id="element" name="element">
-<option value='bloc'>Bloc</option>
-<option value='stab'>Gillet Stabilisateur</option>
-<option value='detendeur'>Détendeur</option>
-<option value='inspecteur_tiv'>Inspecteur TIV</option>
-</select>
-<input type="submit" name="submit" onclick='return(confirm("Procéder à la création ?"));' value="Procéder à la création du nouvel élément"/>
-</form>
-</p>
-<h2>Consultation du journal</h2>
-<div><img src='images/journal.png' style="float: left; margin: 0 15px 0 0;" />
-Consultation des événements du <a href='affichage_element.php?element=journal_tiv'>journal</a>.
+<div class="my-5">
+  <h2 class="title">Déclaration d'un nouvel élément dans la base</h2>
+  
+  <form name="ajout_form" id="ajout_form" action="ajout_element.php" method="POST">
+    <div class="row">
+      <div class="col-auto">
+        <p class=""><i class="fa fa-cogs" aria-hidden="true"></i> Type d'élément à déclarer :</p>
+      </div>
+      <div class="col-12 col-md-3">
+        <select class="custom-select" id="element" name="element">
+          <option value='bloc'>Bloc</option>
+          <option value='stab'>Gillet Stabilisateur</option>
+          <option value='detendeur'>Détendeur</option>
+          <option value='inspecteur_tiv'>Inspecteur TIV</option>
+        </select>
+      </div>
+      <div class="col-12 col-md-3 mt-4 mt-md-0">
+        <input type="submit" class="btn btn-secondary" name="submit" onclick='return(confirm("Procéder à la création ?"));' value="Procéder à la création du nouvel élément"/>
+      </div>
+    </div>
+  </form>
 </div>
-<h2>Consultation d'un TIV</h2>
-<p>
-<img src='images/consultation-tiv.png' style="float: left; margin: 0 15px 0 0;" />
-<form name="consultation_tiv" id="consultation_tiv" action="consultation_tiv.php" method="POST">
-Choisissez votre TIV :
-<select id="date-tiv-consultation" name="date_tiv" onselect="submit()" >
-  <option></option>
-<?php
-include_once("connect_db.inc.php");
-$db_result = $db_con->query("SELECT date, count(id_bloc) FROM inspection_tiv GROUP BY date");
-while($result = $db_result->fetch_array()) {
-  print "  <option value='".$result["date"]."'>".$result["date"]." (".$result[1]." blocs contrôlé(s))</option>\n";
-}
-?>
-</select>
-<input type="submit" name="submit" value="Consulter"/>
-</form>
-</p>
-<h2>Préparation d'un TIV</h2>
+
+<div class="my-5">
+  <h2 class="title">Consultation du journal</h2>
+  <div><p class=""><i class="fa fa-newspaper-o" aria-hidden="true"></i> Consultation des événements du <a class="btn btn-secondary" href='affichage_element.php?element=journal_tiv'>journal</a>.</p>
+  </div>
+</div>
+
+<div class="my-5">
+  <h2 class="title">Consultation d'un TIV</h2>
+  <form name="consultation_tiv" id="consultation_tiv" action="consultation_tiv.php" method="POST">
+    <div class="row">
+      <div class="col-auto">
+        <p class=""><i class="fa fa-check-square" aria-hidden="true"></i> Choisissez votre TIV :</p>
+      </div>
+      <div class="col-12 col-md-3">
+        <select class="custom-select" id="date-tiv-consultation" name="date_tiv" onselect="submit()">
+          <option></option>
+          <?php
+            include_once("connect_db.inc.php");
+            $db_result = $db_con->query("SELECT date, count(id_bloc) FROM inspection_tiv GROUP BY date");
+            while($result = $db_result->fetch_array()) {
+              print "  <option value='".$result["date"]."'>".$result["date"]." (".$result[1]." blocs contrôlé(s))</option>\n";
+            }
+          ?>
+        </select>
+      </div>
+      <div class="col-12 col-md-3 mt-4 mt-md-0">
+        <input type="submit" class="btn btn-secondary" name="submit" onclick='return(confirm("Procéder à la création ?"));' value="Consulter"/>
+      </div>
+    </div>
+  </form>
+  </p>
+</div>
+
+<div class="my-5">
+<h2 class="title">Préparation d'un TIV</h2>
 <script>
   $(function() {
     $('#choix-tiv').hide();
@@ -59,29 +81,30 @@ while($result = $db_result->fetch_array()) {
 <p><img src='images/creation-seance-tiv.png' style="float: left; margin: 0 15px 0 0;" />
 A noter que la préparation d'un TIV consiste à pré-affecter les blocs aux différentes personnes qui feront plus tard le TIV.</p>
 <form name="preparation_tiv" id="preparation_tiv" action="preparation_tiv.php" method="POST">
-<script>
-$(function() {
-  $( "#admin-date-tiv-selector" ).datepicker({
-    changeMonth: true,
-    changeYear: true,
-    dateFormat: 'yy-mm-dd',
-    appendText: '(yyyy-mm-dd)',
+  <script>
+  $(function() {
+    $( "#admin-date-tiv-selector" ).datepicker({
+      changeMonth: true,
+      changeYear: true,
+      dateFormat: 'yy-mm-dd',
+      appendText: '(yyyy-mm-dd)',
+    });
+    $( "#admin-date-tiv-selector" ).datepicker({ altFormat: 'yyyy-mm-dd' });
   });
-  $( "#admin-date-tiv-selector" ).datepicker({ altFormat: 'yyyy-mm-dd' });
-});
-</script>
-<p>Date de préparation du TIV :<input type="text" name="date_tiv" id="admin-date-tiv-selector" size="10" value=""/>
-<input type="submit" name="lancer" value="Procéder à la pré-affectation" /></p>
-<p><a href="#" id="choix-tiv-toggle">Choisir les personnes faisant le TIV</a></p>
-<div id="choix-tiv">
-<select id="tivs" name="tivs[]" multiple='multiple'>
-<?php
-include_once("connect_db.inc.php");
-$db_result = $db_con->query("SELECT id,nom,actif FROM inspecteur_tiv ORDER BY nom");
-while($result = $db_result->fetch_array()) {
-  print "  <option value='".$result["id"]."'".($result["actif"] === "oui" ? " selected" : "").">".$result["nom"]."</option>\n";
-}
-?>
-</select>
-</div>
+  </script>
+  <p>Date de préparation du TIV :<input type="text" name="date_tiv" id="admin-date-tiv-selector" size="10" value=""/>
+  <input type="submit" name="lancer" value="Procéder à la pré-affectation" /></p>
+  <p><a href="#" id="choix-tiv-toggle">Choisir les personnes faisant le TIV</a></p>
+  <div id="choix-tiv">
+    <select id="tivs" name="tivs[]" multiple='multiple'>
+      <?php
+      include_once("connect_db.inc.php");
+      $db_result = $db_con->query("SELECT id,nom,actif FROM inspecteur_tiv ORDER BY nom");
+      while($result = $db_result->fetch_array()) {
+        print "  <option value='".$result["id"]."'".($result["actif"] === "oui" ? " selected" : "").">".$result["nom"]."</option>\n";
+      }
+      ?>
+    </select>
+  </div>
 </form>
+</div>
--- htdocs/index.php
+++ htdocs/index.php
@@ -8,28 +8,41 @@ include_once('head.inc.php');
     $( "#MenuNavigation" ).tabs();
   });
 </script>
-<div id="MenuNavigation">
-  <ul>
-    <li><a href="#accueil"        title="Accueil gestion matériel"       ><img src='images/accueil.png' /> Accueil</a></li>
-    <li><a href="#admin"          title="Administration du site"         ><img src='images/admin.png' /> Administration</a></li>
-    <li><a href="#materiel"       title="Liste du matériel du club"      ><img src='images/materiel.png' /> Matériel</a></li>
-    <li><a href="#personne"       title="Liste des personnes/TIV du club"><img src='images/personne.png' /> Personnes/inspecteurs TIV</a></li>
-    <li><a href="#bloc-rev"       title="Prochaine révisions des blocs"  ><img src='images/etat-bloc.png' /> Status des blocs (TIV/ré-épreuve)</a></li>
-  </ul>
-  <div id="accueil">
-    <?php include("accueil.php");?>
-  </div>
-  <div id="admin">
-    <?php include("admin.php");?>
-  </div>
-  <div id="materiel">
-    <?php include("materiel.php");?>
-  </div>
-  <div id="personne">
-    <?php include("personne.php");?>
-  </div>
-  <div id="bloc-rev">
-    <?php include("affichage_bloc_tiv.php"); ?>
+<div class="container-fluid">
+  <div id="MenuNavigation">
+    <ul class="nav nav-tabs">
+      <li class="nav-item">
+        <a class="nav-link active" id="accueil-tab" data-toggle="tab" href="#accueil" role="tab" aria-controls="accueil" aria-selected="true" title="Accueil gestion matériel" ><img src='images/accueil.png' /> Accueil</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" id="admin-tab" data-toggle="tab" href="#admin" role="tab" aria-controls="admin" aria-selected="true" title="Administration du site"         ><img src='images/admin.png' /> Administration</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" id="materiel-tab" data-toggle="tab" href="#materiel" role="tab" aria-controls="materiel" aria-selected="true" title="Liste du matériel du club" ><img src='images/materiel.png' /> Matériel</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" id="personne-tab" data-toggle="tab" href="#personne" role="tab" aria-controls="personne" aria-selected="true" title="Liste des personnes/TIV du club"><img src='images/personne.png' /> Personnes/inspecteurs TIV</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" id="bloc-rev-tab" data-toggle="tab" href="#bloc-rev" role="tab" aria-controls="bloc-rev" aria-selected="true" title="Prochaine révisions des blocs"><img src='images/etat-bloc.png' /> Status des blocs (TIV/ré-épreuve)</a>
+      </li>
+    </ul>
+    <div class="tab-content">
+    <div class="tab-pane active" role="tabpanel" aria-labelledby="accueil-tab" id="accueil">
+      <?php include("accueil.php");?>
+    </div>
+    <div class="tab-pane" role="tabpanel" aria-labelledby="admin-tab" id="admin">
+      <?php include("admin.php");?>
+    </div>
+    <div class="tab-pane" role="tabpanel" aria-labelledby="materiel-tab" id="materiel">
+      <?php include("materiel.php");?>
+    </div>
+    <div class="tab-pane" role="tabpanel" aria-labelledby="personne-tab" id="personne">
+      <?php include("personne.php");?>
+    </div>
+    <div class="tab-pane" role="tabpanel" aria-labelledby="bloc-rev-tab" id="bloc-rev">
+      <?php include("affichage_bloc_tiv.php"); ?>
+    </div>
   </div>
 </div>
 <?php
