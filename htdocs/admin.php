e09ff9179dac7405e0cb5a960216b406494ce9f9
smeron@lesechos.fr <smeron@lesechos.fr>
2020-10-26 09:00:49 +0100

    ajout appel robinet màj html pour bs4

--- htdocs/admin.php
+++ htdocs/admin.php
@@ -9,6 +9,7 @@
       <div class="col-12 col-md-3">
         <select class="custom-select" id="element" name="element">
           <option value='bloc'>Bloc</option>
+          <option value='robinet'>Robinet</option>
           <option value='stab'>Gillet Stabilisateur</option>
           <option value='detendeur'>Détendeur</option>
           <option value='inspecteur_tiv'>Inspecteur TIV</option>
@@ -32,7 +33,7 @@
   <form name="consultation_tiv" id="consultation_tiv" action="consultation_tiv.php" method="POST">
     <div class="row">
       <div class="col-auto">
-        <p class=""><i class="fa fa-check-square" aria-hidden="true"></i> Choisissez votre TIV :</p>
+        <p class=""><i class="fa fa-book" aria-hidden="true"></i> Choisissez votre TIV :</p>
       </div>
       <div class="col-12 col-md-3">
         <select class="custom-select" id="date-tiv-consultation" name="date_tiv" onselect="submit()">
@@ -55,56 +56,60 @@
 </div>
 
 <div class="my-5">
-<h2 class="title">Préparation d'un TIV</h2>
-<script>
-  $(function() {
-    $('#choix-tiv').hide();
-    $('#choix-tiv-toggle').click(function() {
-      $('#choix-tiv').toggle(400);
-      return false;
-    });
-    $.validator.messages.required = "Champ obligatoire";
-    $("#preparation_tiv").validate({
-      debug: true,
-      rules: {
-        date_tiv: {
-            required: true,
-            date: true,
-        },
-      },
-      submitHandler: function(form) {
-        if(confirm("Lancer la procédure ?")) form.submit();
-      }
-    });
-  });
-</script>
-<p><img src='images/creation-seance-tiv.png' style="float: left; margin: 0 15px 0 0;" />
-A noter que la préparation d'un TIV consiste à pré-affecter les blocs aux différentes personnes qui feront plus tard le TIV.</p>
-<form name="preparation_tiv" id="preparation_tiv" action="preparation_tiv.php" method="POST">
+  <h2 class="title">Préparation d'un TIV</h2>
   <script>
-  $(function() {
-    $( "#admin-date-tiv-selector" ).datepicker({
-      changeMonth: true,
-      changeYear: true,
-      dateFormat: 'yy-mm-dd',
-      appendText: '(yyyy-mm-dd)',
+    $(function() {
+      $('#choix-tiv').hide();
+      $('#choix-tiv-toggle').click(function() {
+        $('#choix-tiv').toggle(400);
+        return false;
+      });
+      $.validator.messages.required = "Champ obligatoire";
+      $("#preparation_tiv").validate({
+        debug: true,
+        rules: {
+          date_tiv: {
+              required: true,
+              date: true,
+          },
+        },
+        submitHandler: function(form) {
+          if(confirm("Lancer la procédure ?")) form.submit();
+        }
+      });
     });
-    $( "#admin-date-tiv-selector" ).datepicker({ altFormat: 'yyyy-mm-dd' });
-  });
   </script>
-  <p>Date de préparation du TIV :<input type="text" name="date_tiv" id="admin-date-tiv-selector" size="10" value=""/>
-  <input type="submit" name="lancer" value="Procéder à la pré-affectation" /></p>
-  <p><a href="#" id="choix-tiv-toggle">Choisir les personnes faisant le TIV</a></p>
-  <div id="choix-tiv">
-    <select id="tivs" name="tivs[]" multiple='multiple'>
-      <?php
-      include_once("connect_db.inc.php");
-      $db_result = $db_con->query("SELECT id,nom,actif FROM inspecteur_tiv ORDER BY nom");
-      while($result = $db_result->fetch_array()) {
-        print "  <option value='".$result["id"]."'".($result["actif"] === "oui" ? " selected" : "").">".$result["nom"]."</option>\n";
-      }
-      ?>
-    </select>
+
+  <div>
+    <p class=""><i class="fa fa-file-text-o" aria-hidden="true"></i> A noter que la préparation d'un TIV consiste à pré-affecter les blocs aux différentes personnes qui feront plus tard le TIV.</p>
   </div>
-</form>
+</div>
+<div class="my-5">
+  <form name="preparation_tiv" id="preparation_tiv" action="preparation_tiv.php" method="POST">
+    <script>
+    $(function() {
+      $( "#admin-date-tiv-selector" ).datepicker({
+        changeMonth: true,
+        changeYear: true,
+        dateFormat: 'yy-mm-dd',
+        appendText: '(yyyy-mm-dd)',
+      });
+      $( "#admin-date-tiv-selector" ).datepicker({ altFormat: 'yyyy-mm-dd' });
+    });
+    </script>
+    <p>Date de préparation du TIV :<input type="text" name="date_tiv" id="admin-date-tiv-selector" size="10" value=""/>
+    <input type="submit" name="lancer" value="Procéder à la pré-affectation" /></p>
+    <p><a href="#" id="choix-tiv-toggle">Choisir les personnes faisant le TIV</a></p>
+    <div id="choix-tiv">
+      <select id="tivs" name="tivs[]" multiple='multiple'>
+        <?php
+        include_once("connect_db.inc.php");
+        $db_result = $db_con->query("SELECT id,nom,actif FROM inspecteur_tiv ORDER BY nom");
+        while($result = $db_result->fetch_array()) {
+          print "  <option value='".$result["id"]."'".($result["actif"] === "oui" ? " selected" : "").">".$result["nom"]."</option>\n";
+        }
+        ?>
+      </select>
+    </div>
+  </form>
 </div>
