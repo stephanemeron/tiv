6e02cd0d40256284979477302440982e39f65c12
smeron@lesechos.fr <smeron@lesechos.fr>
2020-11-01 20:48:14 +0100

    mep menu

--- htdocs/materiel.php
+++ htdocs/materiel.php
@@ -1,8 +1,9 @@
-<h2>Matériels du club</h2>
-
-<ul>
-  <li><a href='affichage_element.php?element=bloc'>Afficher les blocs du club</a></li>
-  <li><a href='affichage_element.php?element=robinet'>Afficher les robinets du club</a></li>
-  <li><a href='affichage_element.php?element=detendeur'>Afficher les détendeurs du club</a></li>
-  <li><a href='affichage_element.php?element=stab'>Afficher les gillets/stabs du club</a></li>
-</ul>
+<h2 class="title my-5">Matériels du club</h2>
+<div class="row ml-3">
+	<ul class="list-group col-12 col-md-4">
+	  <li class="list-group-item"><a href='affichage_element.php?element=bloc'>Afficher les blocs du club</a></li>
+	  <li class="list-group-item"><a href='affichage_element.php?element=robinet'>Afficher les robinets du club</a></li>
+	  <li class="list-group-item"><a href='affichage_element.php?element=detendeur'>Afficher les détendeurs du club</a></li>
+	  <li class="list-group-item"><a href='affichage_element.php?element=stab'>Afficher les gillets/stabs du club</a></li>
+	</ul>
+</div>
