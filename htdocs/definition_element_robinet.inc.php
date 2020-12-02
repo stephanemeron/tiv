e1c6204880ac23271ad9f66ec0f45c6c9d82f820
smeron@lesechos.fr <smeron@lesechos.fr>
2020-12-02 22:13:27 +0100

    màj suite màj sql

--- htdocs/definition_element_robinet.inc.php
+++ htdocs/definition_element_robinet.inc.php
@@ -8,12 +8,15 @@ class robinetElement extends TIVElement {
     $this->_creation_label = "Création d'un robinet";
     $this->_update_label = "Mettre à jour le robinet";
     $this->_elements =  array("id" => "Réf.", "marque" => "Marque", "serial_number"=>" Numéro de série",  "filetage" => "Filetage",
-                              "nb_sortie" => "Nombre de sortie(s)", "observation" => "Observations/Remarques");
+                              "nb_sortie" => "Nombre de sortie(s)", "spec_robinet"=>"Spec SS, DSA, DSS", "net_ultrason" => "Nettoyage ultrason", "observation" => "Observations/Remarques");
+    $spec_robinet = array("SS", "DSA", "DSS");
     $this->_forms = array(
       "marque"        => array("required", "number", "Marque du robinet"),
       "serial_number" => array("required", "text", "Numéro de série du robinet"),
       "filetage"      => array("required", "text" , "Pas du filetage"),
       "nb_sortie"     => array("required", array(1,2) , "Nombre de sortie(s)"),
+      "net_ultrason"     => array("required", "boolean" , "Nettoyage ultrason"),
+      "spec_robinet"     => array("required",$spec_robinet , "Spec"),
       "observation"   => array("required", "text" , "Observations/Remarques"),
     );
     $this->_forms_rules = '
