0cb54db2cce94c61d8520c9800aeb7784adf0f88
smeron@lesechos.fr <smeron@lesechos.fr>
2020-12-15 08:22:42 +0100

    màj mineures

--- htdocs/definition_element_robinet.inc.php
+++ htdocs/definition_element_robinet.inc.php
@@ -2,11 +2,11 @@
 class robinetElement extends TIVElement {
   function robinetElement($db_con = false) {
     parent::__construct($db_con);
-    $this->_parent_url       = "./#materiel-tab";
-    $this->_parent_url_label = "<img src='images/materiel.png' /> Matériel";
     $this->_show_delete_form = true;
-    $this->_creation_label = "Création d'un robinet";
+    $this->_creation_label = "Création d'un robinet".$this->_name;
     $this->_update_label = "Mettre à jour le robinet";
+    $this->_parent_url       = "./#materiel";
+    $this->_parent_url_label = "<img src='images/materiel.png' /> Matériel";
     $this->_elements =  array("id" => "Réf.", "marque" => "Marque", "serial_number"=>" Numéro de série",  "filetage" => "Filetage",
                               "nb_sortie" => "Nombre de sortie(s)", "spec_robinet"=>"Spec SS, DSA, DSS", "net_ultrason" => "Nettoyage ultrason", "observation" => "Observations/Remarques");
     $spec_robinet = array("SS", "DSA", "DSS");
