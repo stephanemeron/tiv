d484ab5e83e9dc2bf70532f50a477e228d2c8476
smeron@lesechos.fr <smeron@lesechos.fr>
2021-02-09 20:43:49 +0100

    Ajout import tableau des filetages des blocs Ajout filetage de sortie

--- htdocs/definition_element_robinet.inc.php
+++ htdocs/definition_element_robinet.inc.php
@@ -8,13 +8,15 @@ class robinetElement extends TIVElement {
     $this->_parent_url       = "./#materiel";
     $this->_parent_url_label = "<img src='images/materiel.png' /> Matériel";
     $this->_elements =  array("id" => "Réf.", "marque" => "Marque", "serial_number"=>" Numéro de série",  "filetage" => "Filetage",
-                              "nb_sortie" => "Nombre de sortie(s)", "spec_robinet"=>"Spec SS, DSA, DSS", "net_ultrason" => "Nettoyage ultrason", "observation" => "Observations/Remarques");
+                              "nb_sortie" => "Nombre de sortie(s)", "filetage_sortie" => "Filetage de sortie", "spec_robinet"=>"Spec SS, DSA, DSS", "net_ultrason" => "Nettoyage ultrason", "observation" => "Observations/Remarques");
     $spec_robinet = array("SS", "DSA", "DSS");
+    $robinet_filetage_sortie = array("G5/8 ISO 228-1", "M26x2 (Nitrox)");
     $this->_forms = array(
       "marque"        => array("required", "number", "Marque du robinet"),
       "serial_number" => array("required", "text", "Numéro de série du robinet"),
-      "filetage"      => array("required", "text" , "Pas du filetage"),
+      "filetage"      => array("required", $this->bloc_filetage , "Pas du filetage"),
       "nb_sortie"     => array("required", array(1,2) , "Nombre de sortie(s)"),
+      "filetage_sortie" => array("required", $robinet_filetage_sortie , "Filetage de sortie"),
       "net_ultrason"     => array("required", "boolean" , "Nettoyage ultrason"),
       "spec_robinet"     => array("required",$spec_robinet , "Spec"),
       "observation"   => array("required", "text" , "Observations/Remarques"),
@@ -32,6 +34,9 @@ class robinetElement extends TIVElement {
         required: true
     },
     nb_sortie: {
+        required: true
+    },
+    filetage_sortie: {
         required: true
     },
   }';
