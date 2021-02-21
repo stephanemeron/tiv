b76bb35ef76a12c9d984bf415f17446eb6f9508f
smeron@lesechos.fr <smeron@lesechos.fr>
2021-02-21 09:21:18 +0100

    ajout readonly_column mise en conformité pour les champs select, radio suivany fonction déjà existante

--- htdocs/definition_element_robinet.inc.php
+++ htdocs/definition_element_robinet.inc.php
@@ -11,14 +11,15 @@ class robinetElement extends TIVElement {
                               "nb_sortie" => "Nombre de sortie(s)", "filetage_sortie" => "Filetage de sortie", "spec_robinet"=>"Spec SS, DSA, DSS", "net_ultrason" => "Nettoyage ultrason", "observation" => "Observations/Remarques");
     $spec_robinet = array("SS", "DSA", "DSS");
     $robinet_filetage_sortie = array("G5/8 ISO 228-1", "M26x2 (Nitrox)");
+    $this->_readonly_column = array("marque", "serial_number", "filetage", "nb_sortie", "filetage_sortie", "spec_robinet");
     $this->_forms = array(
       "marque"        => array("required", "number", "Marque du robinet"),
       "serial_number" => array("required", "text", "Numéro de série du robinet"),
-      "filetage"      => array("required", $this->bloc_filetage , "Pas du filetage"),
-      "nb_sortie"     => array("required", array(1,2) , "Nombre de sortie(s)"),
-      "filetage_sortie" => array("required", $robinet_filetage_sortie , "Filetage de sortie"),
+      "filetage"      => array("required", "radio", "Pas du filetage", $this->bloc_filetage ),
+      "nb_sortie"     => array("required", "radio", "Nombre de sortie(s)",array(1,2) ),
+      "filetage_sortie" => array("required", "radio", "Filetage de sortie", $robinet_filetage_sortie ),
       "net_ultrason"     => array("required", "boolean" , "Nettoyage ultrason"),
-      "spec_robinet"     => array("required",$spec_robinet , "Spec"),
+      "spec_robinet"     => array("required","radio", "Spec", $spec_robinet ),
       "observation"   => array("required", "text" , "Observations/Remarques"),
     );
     $this->_forms_rules = '
