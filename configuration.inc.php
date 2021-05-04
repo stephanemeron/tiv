d388902f9275703c8e3da891f47ada5255278ae0
Stéphane Méron <smeron@lesechos.fr>
2021-05-04 18:51:46 +0200

    màj params globaux

--- configuration.inc.php
+++ configuration.inc.php
@@ -3,7 +3,14 @@ $nom_club     = "Némo";
 $numero_club  = "XX.XX.XXXX";
 $adresse_club = "Adresse du club\nCode postal Ville";
 $logo_club    = "/images/nemo.png";
+$favicon    = "/images/favicon.ico";
 $email_club   = "email@club.com";
+
+// Params pour les durées de Validité pour le TIV
+$epreuve_month_count = 60;
+$epreuve_month_count_warn = 55;
+$tiv_month_count = 12;
+$tiv_month_count_warn = 11;
 
 // Données bloc
 $bloc_capacite = array("6", "10", "12 long", "12 court", "15");
@@ -20,8 +27,15 @@ $bloc_filetage = array("M 25 x 2 ISO", "25 x 200 SI", "G 3/4 DIN 259", "E17 Coni
 $robinet_filetage_sortie = array("G5/8 ISO 228-1", "M26x2 (Nitrox)");
 $spec_robinet = array("SS", "DSA", "DSS");
 
-$epreuve_month_count = 60;
-$epreuve_month_count_warn = 55;
-$tiv_month_count = 12;
-$tiv_month_count_warn = 11;
+// personneElement$qualifications_label = array("", "Nitrox", "Nitrox confirmé", "BIO AFBS", "BIO IFBS",
+$qualifications_label = array("", "Nitrox", "Nitrox confirmé", "BIO AFBS", "BIO IFBS",
+                              "Bio 1", "Bio 2", "E1", "E2", "MFB1", "MFB2", "TIV", "RIFAP");
+$niveau = array("", "Débutant", "Niveau 1", "Niveau 2", "Niveau 2 Initiateur", "Niveau 3", "Niveau 3 Initiateur", "Niveau 4", "Niveau 4 Initiateur", "Moniteur MF1", "Moniteur MF2");
+$assurance = array("", "1", "2", "3");
+
+// Stabs
+$stab_taille = array("junior", "XS", "S", "M", "M/L", "L", "XL", "XXL");
+
+// Prêts
+$etat_possible = array("", "Sortie", "Rentré");
 ?>
