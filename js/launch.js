4e8ef8d48b4ca68469b3f41650f83f96110a00bd
Stéphane Méron <smeron@lesechos.fr>
2021-06-15 22:33:45 +0200

    màj fonction pour comportement navigation

--- js/launch.js
+++ js/launch.js
@@ -4,4 +4,21 @@ $(document).ready(function() {
     		theme: 'bootstrap4'
     	}
     );
+
+    function getParameter(p)
+    {
+        var url = window.location.search.substring(1);
+        var varUrl = url.split('&');
+        for (var i = 0; i < varUrl.length; i++)
+        {
+            var parameter = varUrl[i].split('=');
+            if (parameter[0] == p)
+            {
+                return parameter[1];
+            }
+        }
+    }
+
+
+    $('#navbarSupportedContent a[href="#'+getParameter('pills')+'"]').tab('show');
 });
