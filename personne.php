3a4b8431bd546dfe96846773735c7619350b4a72
Stéphane Méron <smeron@lesechos.fr>
2021-06-15 22:32:56 +0200

    màj html

--- personne.php
+++ personne.php
@@ -1,11 +1,24 @@
-<h1 class="title mb-5"><i class='fa fa-graduation-cap fa-2x'></i> Personnes/inspecteurs TIV</h1>
-
-<h2 class="title mb-3"><i class='fa fa-users'></i> Liste des personnes du club</h2>
-
-<a href='affichage_element.php?element=inspecteur_tiv' class='btn btn-outline-primary'>Afficher les inspecteurs TIV du club</a>
-<a href='affichage_element.php?element=personne' class='btn btn-outline-primary'>Afficher les plongeurs du club</a>
-
-
-<h2 class="title my-5"><i class='fa fa-file-text-o'></i> Prêt de matériel</h2>
-
-<a href='affichage_element.php?element=pret' class='btn btn-outline-primary'>Afficher les prêts en cours</a>
+<h1 class="main title mb-5"><i class='fa fa-graduation-cap fa-2x'></i> Personnes/inspecteurs TIV</h1>
+<div class="row justify-content-center">
+    <div class="col-12">
+        <h2 class="title mb-3 text-center"><i class='fa fa-users'></i> Liste des personnes du club</h2>
+    </div>
+    <div class="col-12 col-md-6 d-flex flex-column flex-md-row">
+        <div class=" flex-fill w-100">
+            <a href='affichage_element.php?element=inspecteur_tiv' class='btn btn-outline-primary p-3 mr-0 mr-md-3 w-100'>Afficher les inspecteurs TIV du club</a>
+        </div>
+        <div class=" flex-fill w-100">
+            <a href='affichage_element.php?element=personne' class='btn btn-outline-primary p-3 ml-0 ml-md-3 w-100'>Afficher les plongeurs du club</a>
+        </div>
+    </div>
+</div>
+<div class="row justify-content-center">
+    <div class="col-12">
+        <h2 class="title my-5 text-center"><i class='fa fa-file-text-o'></i> Prêt de matériel</h2>
+    </div>
+	<div class="col-12 col-md-6 d-flex flex-column flex-md-row">
+        <div class=" flex-fill w-100">
+            <a href='affichage_element.php?element=pret' class='btn btn-outline-primary flex-fill w-100 p-3'>Afficher les prêts en cours</a>
+        </div>
+    </div>
+</div>
