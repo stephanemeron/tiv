634fec31684bde6fa5f8f86c268cd5768366092d
Stéphane Méron <smeron@lesechos.fr>
2021-10-14 21:04:05 +0200

    [WIP] evo html

--- personne.php
+++ personne.php
@@ -1,14 +1,16 @@
-<h1 class="main title mb-5"><i class='fa fa-graduation-cap fa-2x'></i> Personnes/inspecteurs TIV</h1>
+<h1 class="main title mb-5"><i class='fa fa-users fa-2x'></i> Plongeurs/inspecteurs TIV</h1>
 <div class="row justify-content-center">
     <div class="col-12">
-        <h2 class="title mb-3 text-center"><i class='fa fa-users'></i> Liste des personnes du club</h2>
+        <h2 class="title my-5 text-center"><i class='fa fa-address-book-o'></i> Liste des personnes du club</h2>
     </div>
-    <div class="col-12 col-md-6 d-flex flex-column flex-md-row">
-        <div class=" flex-fill w-100">
-            <a href='affichage_element.php?element=inspecteur_tiv' class='btn btn-outline-primary p-3 mr-0 mr-md-3 w-100'>Afficher les inspecteurs TIV du club</a>
-        </div>
-        <div class=" flex-fill w-100">
-            <a href='affichage_element.php?element=personne' class='btn btn-outline-primary p-3 ml-0 ml-md-3 w-100'>Afficher les plongeurs du club</a>
+    <div class="col-12 col-md-6 container">
+        <div class="row">
+            <div class="col-12 col-md-6">
+                <a href='affichage_element.php?element=inspecteur_tiv' class='btn btn-outline-primary p-3 mr-0 mr-md-3 w-100 d-flex align-items-stretch justify-content-center'><i class="fa fa-2x fa-eye"></i><p class="pl-3 mb-0 align-self-center">Afficher les inspecteurs TIV du club</p></a>
+            </div>
+            <div class="col-12 col-md-6">
+                <a href='affichage_element.php?element=personne' class='btn btn-outline-primary p-3 ml-0 ml-md-3 w-100 d-flex align-items-stretch justify-content-center'><i class="fa fa-2x fa-user-circle-o"></i><p class="pl-3 mb-0 align-self-center">Afficher les plongeurs du club</p></a>
+            </div>
         </div>
     </div>
 </div>
@@ -18,7 +20,7 @@
     </div>
 	<div class="col-12 col-md-6 d-flex flex-column flex-md-row">
         <div class=" flex-fill w-100">
-            <a href='affichage_element.php?element=pret' class='btn btn-outline-primary flex-fill w-100 p-3'>Afficher les prêts en cours</a>
+            <a href='affichage_element.php?element=pret' class='btn btn-outline-primary p-3 w-100 d-flex align-items-stretch justify-content-center'><i class="fa fa-2x fa-list-alt"></i><p class="pl-3 mb-0 align-self-center">Afficher les prêts en cours</p></a>
         </div>
     </div>
 </div>
