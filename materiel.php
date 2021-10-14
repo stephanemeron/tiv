066e657d956e3362788ff1f83942a4692cd41990
Stéphane Méron <smeron@lesechos.fr>
2021-10-14 21:26:01 +0200

    màj html

--- materiel.php
+++ materiel.php
@@ -1,9 +1,9 @@
 <h1 class="main title mb-5"><i class='fa fa-wrench fa-2x'></i> Matériels du club</h1>
-<div class="row justify-content-center">
-	<div class="col-12 col-md-6">
-	  <a href='affichage_element.php?element=bloc' class='col-12 btn btn-lg btn-outline-primary my-3'>Afficher les blocs du club</a>
-	  <a href='affichage_element.php?element=robinet' class='col-12 btn btn-lg btn-outline-primary my-3'>Afficher les robinets du club</a>
-	  <a href='affichage_element.php?element=detendeur' class='col-12 btn btn-lg btn-outline-primary my-3'>Afficher les détendeurs du club</a>
-	  <a href='affichage_element.php?element=stab' class='col-12 btn btn-lg btn-outline-primary my-3'>Afficher les gillets/stabs du club</a>
-	</div>
+<div class="justify-content-center my-5">
+    <div class="jumbotron bg-transparent border border-secondary">
+        	  <a href='affichage_element.php?element=bloc' class='btn btn-outline-primary p-3 mr-0 mr-md-3 w-100 d-flex align-items-stretch justify-content-center'><i class="fa fa-battery-full"></i><p class="pl-3 mb-0 align-self-center">Afficher les blocs du club</p></a>
+        	  <a href='affichage_element.php?element=robinet' class='btn btn-outline-primary p-3 mr-0 mr-md-3 w-100 d-flex align-items-stretch justify-content-center my-5'><p class="pl-3 mb-0 align-self-center">Afficher les robinets du club</p></a>
+        	  <a href='affichage_element.php?element=detendeur' class='btn btn-outline-primary p-3 mr-0 mr-md-3 w-100 d-flex align-items-stretch justify-content-center my-5'><i class="fa fa-2x fa-stethoscope"></i><p class="pl-3 mb-0 align-self-center">Afficher les détendeurs du club</p></a>
+        	  <a href='affichage_element.php?element=stab' class='btn btn-outline-primary p-3 mr-0 mr-md-3 w-100 d-flex align-items-stretch justify-content-center'><p class="pl-3 mb-0 align-self-center">Afficher les gillets/stabs du club</p></a>
+    </div>
 </div>
--- personne.php
+++ personne.php
@@ -1,11 +1,9 @@
 <h1 class="main title mb-5"><i class='fa fa-users fa-2x'></i> Plongeurs/inspecteurs TIV</h1>
-<div class="row justify-content-center">
-    <div class="col-12">
-        <h2 class="title my-5 text-center"><i class='fa fa-address-book-o'></i> Liste des personnes du club</h2>
-    </div>
-    <div class="col-12 col-md-6 container">
+<div class="justify-content-center my-5">
+    <div class="jumbotron bg-transparent border border-secondary">
+        <h2 class="title mb-5 text-center"><i class='fa fa-address-book-o'></i> Liste des personnes du club</h2>
         <div class="row">
-            <div class="col-12 col-md-6">
+            <div class="col-12 col-md-6 mb-3 mb-md-0">
                 <a href='affichage_element.php?element=inspecteur_tiv' class='btn btn-outline-primary p-3 mr-0 mr-md-3 w-100 d-flex align-items-stretch justify-content-center'><i class="fa fa-2x fa-eye"></i><p class="pl-3 mb-0 align-self-center">Afficher les inspecteurs TIV du club</p></a>
             </div>
             <div class="col-12 col-md-6">
@@ -14,13 +12,13 @@
         </div>
     </div>
 </div>
-<div class="row justify-content-center">
-    <div class="col-12">
-        <h2 class="title my-5 text-center"><i class='fa fa-file-text-o'></i> Prêt de matériel</h2>
-    </div>
-	<div class="col-12 col-md-6 d-flex flex-column flex-md-row">
-        <div class=" flex-fill w-100">
-            <a href='affichage_element.php?element=pret' class='btn btn-outline-primary p-3 w-100 d-flex align-items-stretch justify-content-center'><i class="fa fa-2x fa-list-alt"></i><p class="pl-3 mb-0 align-self-center">Afficher les prêts en cours</p></a>
+<div class="justify-content-center my-5">
+    <div class="jumbotron bg-transparent border border-secondary">
+        <h2 class="title mb-5 text-center"><i class='fa fa-file-text-o'></i> Prêt de matériel</h2>
+	    <div class="row">
+            <div class="col-12">
+                <a href='affichage_element.php?element=pret' class='btn btn-outline-primary p-3 w-100 d-flex align-items-stretch justify-content-center'><i class="fa fa-2x fa-list-alt"></i><p class="pl-3 mb-0 align-self-center">Afficher les prêts en cours</p></a>
+            </div>
         </div>
     </div>
 </div>
