ae7d2727a8a0c328446c806cfe792723a222e2e3
Stéphane Méron <smeron@lesechos.fr>
2021-10-14 21:04:29 +0200

    màj html header

--- accueil.php
+++ accueil.php
@@ -1,5 +1,5 @@
 <div class="jumbotron bg-transparent border border-secondary my-3">
-	<div class="row">
+	<div class="row align-items-center">
 		<div class="col-12 col-md-3">
 			<img class="img-fluid" src='<?php print $logo_club; ?>' />
 		</div>
