151cb5d4dd451767555114c5ce7994c898c09e89
Stéphane Méron <smeron@lesechos.fr>
2021-10-27 15:08:13 +0200

    màj pour recalcul select2

--- js/launch.js
+++ js/launch.js
@@ -4,6 +4,14 @@ $(document).ready(function() {
     		theme: 'bootstrap4'
     	}
     );
+    $(window).on('resize', function(){
+        setTimeout(
+            $('.my-custom-select').select2(
+            	{
+            		theme: 'bootstrap4'
+            	}
+            ), 200);
+    });
 
     function getParameter(p)
     {
